de3abee00bf20303364db3025af51a3a69209b83
fgrimau <florent.grimau@ulb.be>
2020-02-11 18:58:49 +0100

    [Add] headers for server based classes

---
+++ Server/includes/connected_player.hpp
@@ -0,0 +1,15 @@
+#ifndef ConnectedPlayer_hpp
+    #define ConnectedPlayer_hpp
+
+#include <iostream>
+
+class ConnectedPlayer{
+    public:
+        ConnectedPlayer();
+        ~ConnectedPlayer();
+
+    private:
+        std::string m_name;
+};
+
+#endif
---
+++ Server/includes/sql_parser.hpp
@@ -0,0 +1,14 @@
+#ifndef sql_parser_hpp
+    #define sql_parser_hpp
+
+
+class SQLParser{
+    public:
+        SQLParser();
+        ~SQLParser();
+
+    private:
+
+};
+
+#endif
