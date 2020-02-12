7ad7fca753656ea10683d2151db19616b8331c6d
fgrimau <florent.grimau@ulb.be>
2020-02-12 21:57:46 +0100

    [Upd] Added last table + cleaned code

--- Server/init_db.cpp
+++ Server/init_db.cpp
@@ -1,25 +1,32 @@
+/*
+    File : init_db.cpp
+    Purpose : Creates the initial database and its tables
+    Author : GRIMAU Florent
+    Last Update : 12/02/20
+*/
+
 #include <iostream>
 #include <cstring>
 
 #include "lib/sqlite3.h"
 
 int main() {
-    sqlite3 *db;
-    int rc;
-    char *zErrMsg = nullptr;
-    std::string sql;
+    sqlite3 *db; // The database object
+    std::string sql; // String that will contain the SQL query
 
+    int rc;
+    char* zErrMsg = nullptr;
+
+    // Opening the database (Creating it if the file does not exist)
     rc = sqlite3_open("lab.db", &db);
 
-    if(rc) {
+    if(rc){
         std::cout << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
         return EXIT_FAILURE;
     }
-    else {
-        std::cout << "Opened database successfully" << std::endl;
-    }
+    std::cout << "Database opened" << std::endl << std::endl;
 
-    /* Create SQL statement */
+    std::cout << "------- Creating Users Table -------" << std::endl << ">> ";
     sql = "CREATE TABLE users(\
         id             INTEGER  PRIMARY KEY AUTOINCREMENT,\
         username       CHAR(40) NOT NULL,\
@@ -27,20 +34,17 @@ int main() {
         victory_amount INT\
     );";
 
-    std::cout << "Creating Users Table" << std::endl;
-
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &zErrMsg);
+    rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &zErrMsg);
 
     if(rc != SQLITE_OK){
-       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+       std::cout << "SQL error: " << zErrMsg << std::endl;
        sqlite3_free(zErrMsg);
     }
-    else {
-       fprintf(stdout, "Table created successfully\n");
+    else{
+        std::cout << "Table created successfully" << std::endl;
     }
 
-    /* Create SQL statement */
+    std::cout << "------ Creating History Table ------" << std::endl << ">> ";
     sql = "CREATE TABLE history(\
         id             INTEGER PRIMARY KEY AUTOINCREMENT,\
         user_1_id      INTEGER,\
@@ -54,17 +58,33 @@ int main() {
         timestamp      TIMESTAMP DEFAULT CURRENT_TIMESTAMP\
     );";
 
-    std::cout << "Creating History Table" << std::endl;
-
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &zErrMsg);
+    rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &zErrMsg);
 
     if(rc != SQLITE_OK){
-       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+       std::cout << "SQL error: " << zErrMsg << std::endl;
        sqlite3_free(zErrMsg);
     }
-    else {
-       fprintf(stdout, "Table created successfully\n");
+    else{
+        std::cout << "Table created successfully" << std::endl;
+    }
+
+    std::cout << "----- Creating Messages Table ------" << std::endl << ">> ";
+    sql = "CREATE TABLE messages(\
+        id             INTEGER PRIMARY KEY AUTOINCREMENT,\
+        sender_id      INTEGER,\
+        receiver_id    INTEGER,\
+        content        TEXT,\
+        timestamp      TIMESTAMP DEFAULT CURRENT_TIMESTAMP\
+    );";
+
+    rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &zErrMsg);
+
+    if(rc != SQLITE_OK){
+       std::cout << "SQL error: " << zErrMsg << std::endl;
+       sqlite3_free(zErrMsg);
+    }
+    else{
+        std::cout << "Table created successfully" << std::endl;
     }
 
     sqlite3_close(db);
