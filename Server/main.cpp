4496df57af435ca90e789af349f0864f58ec8248
fgrimau <florent.grimau@ulb.be>
2020-02-12 10:46:08 +0100

    [Mod] Basic server

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,12 +1,68 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <cstdlib>
+#include <netinet/in.h>
+#include <cstring>
+
+#include "includes/utils.hpp"
+
+#define INIT_TAILLE_BUFFER 1024
 
 int main(int argc, char **argv){
-    if(argc != 2){
-        std::cout << "Veuillez indiquer le port que vous souhaiter utiliser" << std::endl;
+
+    struct sockaddr_in server_address;
+
+    int res;
+    uint16_t port;
+    int sockfd;
+
+    char* str_buffer;
+    errno = 0;
+
+    if(argc != 2) {
+        std::cout << "You must provide the port you want to use as a parameter" << std::endl;
         return EXIT_FAILURE;
     }
 
-    std::cout << argv[1] << std::endl;
+    port = static_cast<uint16_t>(strtol(argv[1], NULL, 10));
+
+    if (errno != 0 && port == 0) {
+        perror("Unable to convert port <%s>");
+        return EXIT_FAILURE;
+    }
+
+    server_address.sin_port = htons(port);
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl (INADDR_ANY);
+
+    sockfd = socket (PF_INET, SOCK_STREAM, 0);
+    catch_error(sockfd, 0, "Unable to open socket\n", 0);
+
+    res = bind(sockfd, reinterpret_cast<struct sockaddr *>(&server_address), sizeof (struct sockaddr_in));
+    catch_error(res, 1, "Unable to bind socket and address structure :\n", 1, sockfd);
+
+    res = listen (sockfd, 20);
+    catch_error(res, 0, "Unable to listen.\n", 1, sockfd);
+
+    str_buffer = static_cast<char*>(malloc(sizeof(char) * INIT_TAILLE_BUFFER));
+    if(!str_buffer) {
+        perror("Error while initializing the reception buffer");
+        close (sockfd);
+        return EXIT_FAILURE;
+    }
+
+    while(1) {
+        int socket_client;
+        struct sockaddr_in adresse_client;
+        socklen_t taille_struct_addr_client;
+
+        socket_client = accept(sockfd, reinterpret_cast<struct sockaddr *>(&adresse_client), &taille_struct_addr_client);
+        if (socket_client == -1) {
+            fprintf (stderr, "Connexion impossible.\n");
+            continue;
+        }
+    }
 
     return EXIT_SUCCESS;
 }
