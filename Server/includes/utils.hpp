09b8d9623a179377af4d4deb6fecfafd1b17923e
fgrimau <florent.grimau@ulb.be>
2020-02-12 10:45:50 +0100

    [Add] utils source and headers file

---
+++ Server/includes/utils.hpp
@@ -0,0 +1,6 @@
+#ifndef utils_hpp
+    #define utils_hpp
+
+void catch_error(int res, int is_perror, const char* msg, int nb_to_close, ...);
+
+#endif
---
+++ Server/src/utils.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstdarg>
+
+void catch_error(int res, int is_perror, const char* msg, int nb_to_close, ...){
+    if(res == -1){
+        if(is_perror == 1){
+            perror(msg);
+        }
+        else{
+            fprintf(stderr, "%s", msg);
+        }
+
+        if(nb_to_close > 0){
+            va_list ap;
+            va_start(ap, nb_to_close);
+
+            int i;
+            // traverse rest of the arguments to find out minimum
+            for(i = 2; i <= nb_to_close; i++)
+                close(va_arg(ap, int));
+        }
+        exit(EXIT_FAILURE);
+    }
+}
