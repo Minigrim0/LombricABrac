cfb0ad29e2932c387221e11bcbb23eaae420a8e3
fgrimau <florent.grimau@ulb.be>
2020-02-13 08:48:51 +0100

    [Add] Started source code for database class

---
+++ Server/src/database.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "../includes/database.hpp"
+
+DataBase::DataBase(std::string db_path){
+
+}
+
+DataBase::~DataBase(){
+
+}
+
+void DataBase::get_user(std::string username){
+
+}
+
+void DataBase::register_user(std::string username, std::string password){
+
+}
+
+bool DataBase::verify_user(std::string username, std::string password){
+
+    return false;
+}
