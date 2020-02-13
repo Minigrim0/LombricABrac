51b3a652a931b68444ad3b137ce91529060ca255
fgrimau <florent.grimau@ulb.be>
2020-02-13 21:44:02 +0100

    [Fix] warning treated as error

--- Server/src/database.cpp
+++ Server/src/database.cpp
@@ -34,7 +34,7 @@ bool DataBase::catch_error(){
 
 int callback(void *data, int argc, char **argv, char **azColName){
    int i;
-   fprintf(stderr, "%s: ", (const char*)data);
+   //fprintf(stderr, "%s: ", (const char*)data);
 
    for(i = 0; i<argc; i++){
       printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
