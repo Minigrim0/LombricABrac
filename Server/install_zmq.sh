fbffe9634311b727e1d8ac2e5610d2729c75fc68
fgrimau <florent.grimau@ulb.be>
2020-02-26 00:28:32 +0100

    [Upd] ci instructions + lib installer to pass the pipelines

--- Server/install_zmq.sh
+++ Server/install_zmq.sh
@@ -4,7 +4,11 @@ if [ ! -d "lib/libzmq/" ]; then
     git clone https://github.com/zeromq/libzmq libzmq && cd libzmq;
     mkdir build && cd build;
     cmake ..;
-    sudo make -j4 install;
+    if [ $SUDO -eq 0 ]; then
+        make -j4 install;
+    else
+        sudo make -j4 install;
+    fi
     cd ../../;
 else
     echo "\e[0;34mzmq seems to be installed\e[0m"
@@ -15,7 +19,11 @@ if [ ! -d "lib/cppzmq/" ]; then
     git clone https://github.com/zeromq/cppzmq cppzmq && cd cppzmq;
     mkdir build && cd build;
     cmake ..;
-    sudo make -j4 install;
+    if [ $SUDO -eq 0 ]; then
+        make -j4 install;
+    else
+        sudo make -j4 install;
+    fi
     cd ../../;
 else
     echo "\e[0;34mcppzmq seems to be installed\e[0m"
