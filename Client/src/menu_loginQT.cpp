487fa00ed1e80d7781d7deb8f48230653701200e
sirenard <renardsimon@skynet.be>
2020-03-19 16:30:37 +0100

    on voit le background

--- Client/src/menu_loginQT.cpp
+++ Client/src/menu_loginQT.cpp
@@ -4,7 +4,7 @@
 
 Menu_LoginQT::Menu_LoginQT(int id, MainWindow *parent, Client* cli):
 WindowQT(id, parent, client){
-
+    setStyleSheet("background-image: url(./UI/Resources/cropped-1920-1080-521477.jpg);");
   Ui::Menu_LoginWidget page;
   page.setupUi(this);
 }
