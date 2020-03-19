e8bd0b8b7d8ebf1d234deede243ae4564ebd7ec3
sirenard <renardsimon@skynet.be>
2020-03-19 13:45:47 +0100

    pour que ça compile correctement

--- Client/src/menu_registerQT.cpp
+++ Client/src/menu_registerQT.cpp
@@ -1,10 +1,9 @@
 #include "../includes/LesFenetres_QT.hpp"
-
 #include "../UI/src/Menu_RegisterQt_ui.hpp"
 
 Menu_RegisterQT::Menu_RegisterQT(int id, MainWindow *parent, Client* cli):
 WindowQT(id, parent, client){
-  Ui::Menu_RegisterWidget page;
+  Ui::Form page;
   //page.setupUi(this);
 
 
