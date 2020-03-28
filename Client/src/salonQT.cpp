8c4523902b3527e8b99f7d477ca090a6116a2f44
isouriag <ismal.ouriaghli@ulb.be>
2020-03-28 16:38:54 +0100

    vérifie si y a bien un joeueur dans une équipe avnt de lancer la partie (genre pas que des spectateurs)

--- Client/src/salonQT.cpp
+++ Client/src/salonQT.cpp
@@ -274,9 +274,24 @@ void SalonQT::update_para(){
 }
 
 void SalonQT::play(){
-  client->startGame();
-  parent->setPage(GAME_SCREEN);
+  vector<playerTeam> joueur_in_room;
+  bool player = false;
+  int len_tab;
+  joueur_in_room = client->getTeams();
+  len_tab = static_cast<int>(joueur_in_room.size());
+
+  for (int i = 0; i< len_tab; i++){
+    if (joueur_in_room[static_cast<unsigned int>(i)].id_team != 0){
+      player = true;
+      break;
+    }
+  }
+  if (player){
+    client->startGame();
+    parent->setPage(GAME_SCREEN);
+  }
 }
+
 
 void SalonQT::change_equipe(){
   int equipe = page->Choix_EquipeSpinBox->value();
